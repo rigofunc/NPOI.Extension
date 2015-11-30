4634daaf336a8d741dc6712008cc803c2f91c83e
rigofunc (xyting) <xuyingting794@tops001.com>
2015-12-01 00:05:33 +0800

    modify the description

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NPOI.Extension")]
-[assembly: AssemblyDescription("Extension of NPOI, that use attributes to control enumerable objects export  to excel behaviors.")]
+[assembly: AssemblyDescription("The extensions of NPOI, which provides IEnumerable<T> save to and load from excel functionality.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("RigoFunc (xuyingting)")]
 [assembly: AssemblyProduct("NPOI.Extension")]
