15c8be40b3733596942b8bf92bfb6012b595a3e5
yingtingxu <yingtingxu@tencent.com>
2017-09-21 10:15:23 +0800

    feat: demo extension point.

---
+++ samples/FluentConfigurationExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace FluentExcel.Samples
+{
+    public static class FluentConfigurationExtensions
+    {
+        public static FluentConfiguration<TModel> FromAnnotations<TModel>(this FluentConfiguration<TModel> fluentConfiguration) where TModel : class
+        {
+            var properties = typeof(TModel).GetProperties();
+            foreach (var property in properties)
+            {
+                var pc = fluentConfiguration.Property(property);
+
+                var display = property.GetCustomAttribute<DisplayAttribute>();
+                if (display != null)
+                {
+                    pc.HasExcelTitle(display.Name);
+                    if (display.GetOrder().HasValue)
+                    {
+                        pc.HasExcelIndex(display.Order);
+                    }
+                }
+                else
+                {
+                    pc.HasExcelTitle(property.Name);
+                }
+
+                var format = property.GetCustomAttribute<DisplayFormatAttribute>();
+                if (format != null)
+                {
+                    pc.HasDataFormatter(format.DataFormatString
+                                              .Replace("{0:", "")
+                                              .Replace("}", ""));
+                }
+
+                if (pc.Index < 0)
+                {
+                    pc.HasAutoIndex();
+                }
+            }
+
+            return fluentConfiguration;
+        }
+    }
+}
--- samples/Report.cs
+++ samples/Report.cs
@@ -1,17 +1,27 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace samples
 {
     public class Report
     {
+        [Display(Name = "城市")]
         public string City { get; set; }
+        [Display(Name = "楼盘")]
         public string Building { get; set; }
-		public string Area { get; set; }
-		public DateTime HandleTime { get; set; }
+        [Display(Name = "区域")]
+        public string Area { get; set; }
+        [Display(Name = "成交时间")]
+        public DateTime HandleTime { get; set; }
+        [Display(Name = "经纪人")]
         public string Broker { get; set; }
+        [Display(Name = "客户")]
         public string Customer { get; set; }
+        [Display(Name = "房源")]
         public string Room { get; set; }
+        [Display(Name = "佣金(元)")]
         public decimal Brokerage { get; set; }
+        [Display(Name = "收益(元)")]
         public decimal Profits { get; set; }
     }
 }
